5bec70812d9d11617b87e5c996270e99ac26e632
Diana Calderon <dianacalderon@ravn.co>
2022-07-23 17:12:30 -0600

    Solving question 16 medium-pop

--- questions/16-medium-pop/test-cases.ts
+++ questions/16-medium-pop/test-cases.ts
@@ -4,3 +4,6 @@ type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
   Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
 ]
+
+//My Example
+const correctPop: Pop<['aqua','green','red']> = ['aqua','green'];
