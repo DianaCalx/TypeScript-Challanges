09a84658fa4355aecbe13b3e2e3585d185aba6ee
Diana Calderon <dianacalderon@ravn.co>
2022-07-23 17:11:46 -0600

    Solving question 15 medium-last

--- questions/15-medium-last/template.ts
+++ questions/15-medium-last/template.ts
@@ -1,1 +1,1 @@
-export type Last<T extends any[]> = any
+export type Last<T extends any[]> = T extends [...infer Values, infer L] ? L : never;
--- questions/15-medium-last/test-cases.ts
+++ questions/15-medium-last/test-cases.ts
@@ -4,3 +4,6 @@ type cases = [
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
   Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
 ]
+
+//My example
+const correctLastArray: Last<['aqua','green','red']> = 'red';
