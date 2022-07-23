0ec0e9f462b8f46d04efb35c2810c37eebd3cb60
Diana Calderon <dianacalderon@ravn.co>
2022-07-23 17:10:49 -0600

    Solving question 10 medium-tuple-to-union

--- questions/10-medium-tuple-to-union/test-cases.ts
+++ questions/10-medium-tuple-to-union/test-cases.ts
@@ -4,3 +4,6 @@ type cases = [
   Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
   Expect<Equal<TupleToUnion<[123]>, 123>>,
 ]
+
+//My Example
+const correctTupleUnion: TupleToUnion<['apple',2.50, false]> = 'apple';
